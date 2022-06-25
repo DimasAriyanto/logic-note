87a0ec4c9798931f5f0ed861b5067f41f542eb38
Ikapurwanti <ikapurwanti@students.amikom.ac.id>
2022-06-25 14:25:14 +0700

    adding migrate

--- database/migrations/2022_06_25_071055_create_tugas_table.php
+++ database/migrations/2022_06_25_071055_create_tugas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTugasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tugas', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('no_wa');
-            $table->rememberToken();
+            $table->foreignId('id_user');
+            $table->string('nama_tugas');
+            // $table->enum('status');
+            $table->dateTime('waktu');
+            $table->teks('deskripsi');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tugas');
     }
 }
